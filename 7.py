8dec64d5390e3c008c8acf03b6ae03e9dbb7c30f
vinuthreddy <43026200+vinuthreddy@users.noreply.github.com>
2018-11-10 12:38:57 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -8,8 +8,17 @@ else:
 	else:
 		print("b is greater than a ")
 		
-
+output
+enter a value 6
+enter b value 9
+b is greater than a 
     
+enter a value 9     
+enter b value 6
+a is greater than b
     
+enter a value 6
+enter b value 6
+both numbers are equal
 
 
