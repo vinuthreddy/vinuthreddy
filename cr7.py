bc341a13d46b1dca723ac641ed9bf0621114d7ac
vinuthreddy <43026200+vinuthreddy@users.noreply.github.com>
2018-12-06 12:59:42 +0530

    Update cr7.py

--- cr7.py
+++ cr7.py
@@ -1,4 +1,5 @@
 #subtraction of 3 numbers
+input
 p=20
 q=60
 r=90
@@ -7,3 +8,9 @@ print("the value of q=",q,)
 print("the value of r=",r,)
 result=p-q-r
 print("subtraction of ",p,q ,"and ",r ,"is", result)
+
+output
+the value of p= 20
+the value of q= 60
+the value of r= 90
+subtraction of  20 60 and  90 is -130
