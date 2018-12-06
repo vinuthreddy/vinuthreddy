ec45caee908be99aabc0df589c7fa64d34c1eae7
vinuthreddy <43026200+vinuthreddy@users.noreply.github.com>
2018-12-06 12:55:32 +0530

    Update ams.py

--- ams.py
+++ ams.py
@@ -1,6 +1,18 @@
+input 
 x=int(input("enter the value of x"))
 y=int(input("enter the value of y"))
 print("addition of",x, "and" ,y, "is", x+y,)
 print("subtraction of",x,"and",y, "is",x-y,)
 print("multiplication of",x,"and",y, "is",x*y,)
 print("division of",x,"and",y, "is",x/y,)
+
+output
+enter the value of x 6
+enter the value of y 3
+addition of 6 and 3 is 9
+subtraction of 6 and 3 is 3
+multiplication of 6 and 3 is 18
+division of 6 and 3 is 2.0
+
+ 
+  
