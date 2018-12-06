4031debc302367c56727b166f751c59aed4f4faf
vinuthreddy <43026200+vinuthreddy@users.noreply.github.com>
2018-12-06 13:01:57 +0530

    Update div.py

--- div.py
+++ div.py
@@ -1,4 +1,5 @@
 #dividing 3 numbers
+input
 a=86
 b=12
 c=6
@@ -7,3 +8,9 @@ print("the value of b=",b,)
 print("the value of c=",c,)
 result=a/b/c
 print("the division of",a,b,"and",c,"is",result,)
+
+output
+the value of a= 86
+the value of b= 12
+the value of c= 6
+the division of 86 12 and 6 is 1.1944444444444444
