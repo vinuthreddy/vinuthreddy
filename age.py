6c404cfb2f5de35cb8c78becf3134006ab3b412a
vinuthreddy <43026200+vinuthreddy@users.noreply.github.com>
2018-12-06 12:53:05 +0530

    Update age.py

--- age.py
+++ age.py
@@ -1,6 +1,12 @@
+input
 age=int(input("enter your age"))
 if(age>=18):
 	print("you are eligible to vote")
 else:
 	print("you are not eligible to vote")
+output
+enter your age 15
+you are not eligible to vote
+enter your age 22
+you are eligible to vote
 
